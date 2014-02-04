6a962d4931d86eba44d1cd806999fe184a49f09a
Michael <mib@michael-baumgaertner.de>
2014-02-04 22:42:46 +0100

    switched to use pytest for tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,15 +1,10 @@
 # coding: utf-8
 
-import unittest, mock
+import pytest, mock
 
 from sitebackup import main
 
-class TestMain(unittest.TestCase):
-  
-  def testHelp(self):
-    with self.assertRaises(SystemExit) as context:
-      result = main(["-h",])
-    self.assertEqual(context.exception.code, 0)
-
-if __name__ == '__main__':
-    unittest.main()
+def testHelp():
+    with pytest.raises(SystemExit) as exceptioninfo:
+        result = main(["-h",])
+    assert exceptioninfo.value.code == 0
