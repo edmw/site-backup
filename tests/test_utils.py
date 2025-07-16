b45c32085d99e31952475f46190998643e7c6976
Michael Baumgärtner <mib@yamanote.de>
2025-07-16 16:21:37 +0200

    fix tests for formatting of binary sizes

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,7 +2,7 @@
 
 import pytest  # noqa: 401
 
-from backup.utils import timestamp4now, timestamp2date, slugify, formatsize
+from backup.utils import formatsize, slugify, timestamp2date, timestamp4now
 
 
 def testTimestamp():
@@ -43,9 +43,9 @@ def testFormatSizeBinary():
     assert formatsize(123123, binary=True) == "120.2 KiB"
     assert formatsize(123123123, binary=True) == "117.4 MiB"
     assert formatsize(123123123123, binary=True) == "114.7 GiB"
-    assert formatsize(123123123123123, binary=True) == "123.1 TB"
-    assert formatsize(123123123123123123, binary=True) == "123.1 PB"
-    assert formatsize(123123123123123123123, binary=True) == "123.1 EB"
-    assert formatsize(123123123123123123123123, binary=True) == "123.1 ZB"
-    assert formatsize(123123123123123123123123123, binary=True) == "123.1 YB"
-    assert formatsize(123123123123123123123123123123, binary=True) == "123123.1 YB"
+    assert formatsize(123123123123123, binary=True) == "112.0 TiB"
+    assert formatsize(123123123123123123, binary=True) == "109.4 PiB"
+    assert formatsize(123123123123123123123, binary=True) == "106.8 EiB"
+    assert formatsize(123123123123123123123123, binary=True) == "104.3 ZiB"
+    assert formatsize(123123123123123123123123123, binary=True) == "101.8 YiB"
+    assert formatsize(123123123123123123123123123123, binary=True) == "101845.1 YiB"
